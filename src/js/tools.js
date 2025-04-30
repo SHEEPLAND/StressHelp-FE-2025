7d7d111b97c998a16704b4d2a2ab8fd7eb677912
abonti <abontimary@gmail.com>
2025-04-30 23:55:15 +0300

    Updates

--- src/js/tools.js
+++ src/js/tools.js
@@ -55,3 +55,17 @@ if (typeof ScrollReveal !== "undefined") {
   console.warn("ScrollReveal library is not loaded.");
 }
 
+const tabButtons = document.querySelectorAll(".tab-btn");
+const tabContents = document.querySelectorAll(".tab-content");
+
+tabButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    
+    tabButtons.forEach(btn => btn.classList.remove("active"));
+    tabContents.forEach(content => content.classList.remove("active"));
+
+ 
+    button.classList.add("active");
+    document.getElementById(button.dataset.tab).classList.add("active");
+  });
+});
